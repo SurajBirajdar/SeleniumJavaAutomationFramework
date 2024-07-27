c0f1a08a85ecabd1100c19f99af9a8a4c3f242e0
SurajBirajdar <surajbirajdar0371@gmail.com>
2024-07-28 00:13:54 +0530

    Added JenkinsTest class and updated pom.xml, testng.xml with respect to it.

---
+++ src/test/java/testcases/JenkinsTest.java
@@ -0,0 +1,11 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class JenkinsTest {
+
+    @Test
+    public void jenkinsTest() {
+        System.out.println("Jenkins test done successfully");
+    }
+}
